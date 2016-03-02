91d46eab49f866961905e868e1ceb285a214ddab
Joe Price <joe@opendoor.com>
2016-03-01 20:42:39 -0800

    Fix setInterval use

--- app/assets/client/js/common/interval-timer.js
+++ app/assets/client/js/common/interval-timer.js
@@ -1,42 +1,20 @@
-let getCurrentMs = () => {
-    return new Date().getTime();
-};
-
 export default class {
   constructor(interval, func) {
     this.interval = interval;
     this.func = func;
-    this.currentTime = getCurrentMs();
-
-    this.isPlaying = false;
-    this.shouldStop = true;
+    this._intervalId = null;
   }
 
   start() {
-    this.shouldStop  = false;
-
-    if (!this.isPlaying) {
-      this.isPlaying = true;
-      this.doInterval();
+    if (this._intervalId == null) {
+      this._intervalId = setInterval(this.func, this.interval);
     }
   }
 
   stop() {
-    if (this.isPlaying) {
-      this.shouldStop = true;
+    if (this._intervalId != null) {
+      clearInterval(this._intervalId);
+      this._intervalId = null;
     }
-  }
-
-  doInterval() {
-    if (this.shouldStop) {
-      this.shouldStop = false;
-      this.isPlaying = true;
-      return;
-    }
-
-    this.func();
-
-    //TODO - handle this more precisely
-    setInterval(this.func, this.interval);
   }
 }
